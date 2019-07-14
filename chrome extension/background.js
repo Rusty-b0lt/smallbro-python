2771c5a361c75ec1e40e9c13b2492725020ff4a9
Rusty-b0lt <pk@islc.org>
2019-07-14 22:26:21 +0300

    changed extension files

---
+++ chrome extension/background.js
@@ -0,0 +1,25 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+var url;
+var socket = new WebSocket("ws://127.0.0.1:5678");
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if(changeInfo.url !== undefined) {
+ 		socket.send('onUpdated,' + changeInfo.url);
+	}
+}); 
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab){
+  	if(tab.url !== undefined) {
+    	socket.send('onActivated,' + tab.url);
+  	}
+  });
+});
+socket.onclose = function(event) {
+  if (event.wasClean) {
+  	socket.send('ConnectionClosed, None')
+  } 
+}
